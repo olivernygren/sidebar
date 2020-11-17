6da945499f3848ed88c3b03d12df0714ea63a287
Oliver Nygren <oliver.nygren@icloud.com>
2020-11-17 10:46:53 +0100

    displayed text when open (temporary)?

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 
 const sidebarButton = document.getElementById('sidebar-btn');
 const sidebar = document.querySelector('.sidebar');
-const hasWidth = getComputedStyle(sidebar);
+const profile = document.querySelector('.profile');
+const categories = document.querySelector('.categories');
+const settings = document.querySelector('.settings');
 
 sidebarButton.addEventListener('click', showSidebar);
 
 function showSidebar() {
 
     if (sidebar.classList.contains('show-sidebar')) {
+
         sidebar.classList.remove('show-sidebar');
+        profile.classList.add('display-none');
+        categories.classList.add('display-none');
+        settings.classList.add('display-none');
+
     } else {
+
         sidebar.classList.add('show-sidebar');
+        profile.classList.remove('display-none');
+        categories.classList.remove('display-none');
+        settings.classList.remove('display-none');
     }
 
 }
